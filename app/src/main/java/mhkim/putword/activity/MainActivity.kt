ad9f823fd6a15bf11daaad04968e5143f0d9272f
kimmyunghoon <kl951995@gmail.com>
2022-04-23 14:51:59 +0900

    [초기] 안드로이드 코틀린 프로젝트 초기 생성

---
+++ app/src/main/java/mhkim/putword/activity/MainActivity.kt
@@ -0,0 +1,14 @@
+package mhkim.putword.activity
+
+import android.app.Activity
+import android.os.Bundle
+import mhkim.putword.R
+
+class MainActivity : Activity(){
+
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_main)
+
+    }
+}
