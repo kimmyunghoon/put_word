dd4bc13ccdeab79c66dd31c3988a9d6cb8fa6d26
kimmyunghoon <kl951995@gmail.com>
2022-04-23 16:12:18 +0900

    [수정] 예제 등록 화면

--- app/src/main/java/mhkim/putword/activity/MainActivity.kt
+++ app/src/main/java/mhkim/putword/activity/MainActivity.kt
@@ -5,10 +5,11 @@ import android.os.Bundle
 import mhkim.putword.R
 
 class MainActivity : Activity(){
-
+    //Todo : AnimatedVectorDrawable 사용 예제 구현해볼것
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
+
 
     }
 }
